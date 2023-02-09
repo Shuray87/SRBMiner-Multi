9945f9ad55ab6ac27283db6b24ba13ca5c0a4536
Shuray87 <101868588+Shuray87@users.noreply.github.com>
2023-02-09 11:43:02 +0700

    Create guided-setup.sh

---
+++ guided-setup.sh
@@ -0,0 +1,56 @@
+= Guided setup =
+Configuration name:
+: veruscoin (nama start yang anda inginkan)
+Do you want to use multi algorithm mining ?
+ly-yes, n-no]
+:n
+Available algorithms : pilihan algoritma!
++ argon2d_dynamic
++ argon2id_chukwa
++argon2id_chukwa2
++ argon2id_ninja
++ autolykos2
++ balloon_zentoshi
++bl2bsha3
++ blake2b
++blake2s
++ circcash
+www.galaxyite.com/2021/06/cara-menggur 11:37 AMOODe
++ cryptonight_cache
++cryptonight_ccx
++ cryptonight_gpu
++ cryptonight_heavyx
++ cryptonight_talleo
++cryptonight_upx
++ cryptonight_xhv
++ curvehash
++ eaglesong
++ etchash
++ ethash
++k12
++ kadena
++ keccak
++ minotaur
++panthera
++ phi5
++ randomarq
++ randomepic
++randomhash2
++randomkeva
++ randomsfx
++ randomwow
++randomx
++randomx/
++scryptn2
++ ubqhash
++ verushash
++ yescrypt
++ yescryptr16
++ yescryptr32
++ yescryptr8
++ yespower
++ yespower2b
++ yespoweric
++yespoweriots
++yespoweritc
++ yespowerlitb
